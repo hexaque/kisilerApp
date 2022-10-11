ba05d92c82268f5216aa0cf83c98c6ddb60b4c92
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-11 19:17:21 +0300

    Update AnasayfaInteractor.swift
    db kisi sil kişi ara eklendi

--- KisilerUygulamasi/anasayfa-module/interactor/AnasayfaInteractor.swift
+++ KisilerUygulamasi/anasayfa-module/interactor/AnasayfaInteractor.swift
@@ -35,11 +35,41 @@ class AnasayfaInteractor : PresenterToInteractorAnasayfaProtocol{
     }
     
     func kisiAra(aramaKelimesi: String) {
-        print("kişi Ara : \(aramaKelimesi)")
+        
+            var liste = [Kisiler]()
+            
+            db?.open()
+            do{
+                let r = try db!.executeQuery("SELECT * FROM kisiler WHERE kisi_ad like '%\(aramaKelimesi)%' ", values: nil)
+                while r.next(){
+                    let kisi = Kisiler(kisi_id:Int(r.string(forColumn: "kisi_id"))! ,kisi_ad: r.string(forColumn: "kisi_ad")!, kisi_tel: r.string(forColumn: "kisi_tel")!)
+                    liste.append(kisi)
+                }
+                anasayfaPresenter?.presenteraVeriGonder(kisilerListesi: liste)
+            }catch{
+                print(error.localizedDescription)
+            }
+            db?.close()
+
+        
+       // print("kişi Ara : \(aramaKelimesi)")
     }
     
     func kisiSil(kisi_id: Int) {
-        print("kişi sil : \(kisi_id)")
+        db?.open()
+        do{
+            try db!.executeUpdate("DELETE FROM kisiler WHERE kisi_id = ?", values: [kisi_id])
+            tumKisileriAl()
+            }catch{
+            
+        
+            print(error.localizedDescription)
+        }
+        db?.close()
+        
+        
+        
+        //print("kişi sil : \(kisi_id)")
     }
     
     
